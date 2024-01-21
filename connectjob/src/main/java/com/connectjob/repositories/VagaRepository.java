d13a22a664298546d544770073e64407a7424aaf
Stéfany Marques <stefany.marques06@gmail.com>
2024-01-21 18:56:23 -0300

    Ajustes de Controller, Repository, Services e Services Implements da tabela Vaga

--- connectjob/src/main/java/com/connectjob/repositories/VagaRepository.java
+++ connectjob/src/main/java/com/connectjob/repositories/VagaRepository.java
@@ -3,12 +3,14 @@ package com.connectjob.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.connectjob.model.Usuario;
 import com.connectjob.model.Vaga;
+
+
 
 @Repository
 public interface VagaRepository extends JpaRepository<Vaga, Long>{
@@ -17,8 +19,9 @@ public interface VagaRepository extends JpaRepository<Vaga, Long>{
 
 	List<Vaga> findCandidatoById(Long candidatoId);
 	
-	// List<Usuario> findUsuariosById(Long vagaId);
+	List<Vaga> findByArea(String area);
 	
 	@Query("SELECT v.usuario FROM Vaga v WHERE v.id = :vagaId")
     List<Usuario> findUsuarioById(Long vagaId);
+
 }
